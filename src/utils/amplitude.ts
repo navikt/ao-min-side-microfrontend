38c4ad44699907253cd1be6a1ba9bcff5f3cb8ed
erikbosnes <erik.bosnes@nav.no>
2025-02-17 12:29:43 +0100

    start

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -3,5 +3,4 @@ import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
 const logger = getAmplitudeInstance("dekoratoren");
 
 //TODO: komponent må fylles ut med et identifiserende navn for microfrontend
-//Default er at navnet på appen blir benyttet ved replace av "tms-microfrontend-template"
-export const logNavigereEvent = () => logger("navigere", { komponent: "tms-microfrontend-template" });
+export const logNavigereEvent = () => logger("navigere", { komponent: "ao-min-side-microfrontend" });
