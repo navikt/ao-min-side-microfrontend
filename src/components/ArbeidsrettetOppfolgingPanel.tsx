1f49efef3eb500e6472ad3ca3a512a5d466022bd
johannetronstad <johanne.tronstad@nav.no>
2025-02-18 08:23:32 +0100

    Fiks styling

--- src/components/ArbeidsrettetOppfolgingPanel.tsx
+++ src/components/ArbeidsrettetOppfolgingPanel.tsx
@@ -3,18 +3,22 @@ import { BodyShort, Heading, ReadMore } from "@navikt/ds-react";
 import { LanguageContext } from "../language/LanguageProvider";
 import { useContext } from "react";
 import { text } from "../translations/text";
+import LinkCard from "./card/LinkCard";
 
 const Komponent = () => {
   const language = useContext(LanguageContext);
+  const url = "";
 
   return (
-    <div className={styles.komponent}>
-      <Heading size="small" className={styles.heading}>
-        {text.card.heading[language]}
-      </Heading>
-      <BodyShort>{text.card.description[language]}</BodyShort>
-      <ReadMore header={text.card.readMoreTittel[language]}>{text.card.readMoreInnhold[language]}</ReadMore>
-    </div>
+    <LinkCard href={url}>
+      <div>
+        <Heading size="small" className={styles.heading}>
+          {text.card.heading[language]}
+        </Heading>
+        <BodyShort className={styles.detail}>{text.card.description[language]}</BodyShort>
+        <ReadMore header={text.card.readMoreTittel[language]}>{text.card.readMoreInnhold[language]}</ReadMore>
+      </div>
+    </LinkCard>
   );
 };
 
