2c347c79e8255ed726dcab77711c28657dff07d7
johannetronstad <johanne.tronstad@nav.no>
2025-02-18 08:52:22 +0100

    Fjern deprekert linkpanel komponent

--- src/components/ArbeidsrettetOppfolgingPanel.tsx
+++ src/components/ArbeidsrettetOppfolgingPanel.tsx
@@ -1,24 +1,24 @@
-import styles from "./Komponent.module.css";
+import styles from "./ArbeidsrettetOppfolgingPanel.module.css";
 import { BodyShort, Heading, ReadMore } from "@navikt/ds-react";
 import { LanguageContext } from "../language/LanguageProvider";
 import { useContext } from "react";
 import { text } from "../translations/text";
-import LinkCard from "./card/LinkCard";
 
 const Komponent = () => {
   const language = useContext(LanguageContext);
-  const url = "";
 
   return (
-    <LinkCard href={url}>
-      <div>
+    <div className={styles.box}>
+      <div className={styles.headingDiv}>
         <Heading size="small" className={styles.heading}>
           {text.card.heading[language]}
         </Heading>
+      </div>
+      <div className={styles.infoDiv}>
         <BodyShort className={styles.detail}>{text.card.description[language]}</BodyShort>
         <ReadMore header={text.card.readMoreTittel[language]}>{text.card.readMoreInnhold[language]}</ReadMore>
       </div>
-    </LinkCard>
+    </div>
   );
 };
 
