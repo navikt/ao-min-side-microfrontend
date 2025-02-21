a42592edf767f54c68166c5c4b7b4526b0269de4
erikbosnes <erik.bosnes@nav.no>
2025-02-21 07:55:50 +0100

    Revert formatDate-funksjonen og oppdaterer visning av tidspunkt i ArbeidsrettetOppfolgingPanel

--- src/components/ArbeidsrettetOppfolgingPanel.tsx
+++ src/components/ArbeidsrettetOppfolgingPanel.tsx
@@ -5,17 +5,17 @@ import { useContext, useEffect, useState } from "react";
 import { text } from "../translations/text";
 import { VeilarboppfolgingApi } from "../api/veilarboppfolging";
 
-const formatDate = (date: Date, locale: string): string => {
-  const formatter = new Intl.DateTimeFormat(locale, {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
-  return formatter.format(date);
-};
+// const formatDate = (date: Date, locale: string): string => {
+//   const formatter = new Intl.DateTimeFormat(locale, {
+//     day: "numeric",
+//     month: "long",
+//     year: "numeric",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//   });
+//
+//   return formatter.format(date);
+// };
 
 const ArbeidsrettetOppfolgingPanel = () => {
   const language = useContext(LanguageContext);
@@ -25,19 +25,19 @@ const ArbeidsrettetOppfolgingPanel = () => {
     VeilarboppfolgingApi.hentGjeldendeOppfolgingsperiode().then((response) => setTimestamp(response));
   }, []);
 
-  const startTidspunkt = formatDate(new Date(timestamp), language);
+  // const startTidspunkt = formatDate(new Date(timestamp), language);
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <Heading size="small" className={styles.heading}>
-          {text(startTidspunkt).card.heading[language]}
+          {text(timestamp).card.heading[language]}
         </Heading>
       </div>
       <div className={styles.body}>
-        <BodyShort className={styles.detail}>{text(startTidspunkt).card.description[language]}</BodyShort>
-        <ReadMore header={text(startTidspunkt).card.readMoreTittel[language]}>
-          {text(startTidspunkt).card.readMoreInnhold[language]}
+        <BodyShort className={styles.detail}>{text(timestamp).card.description[language]}</BodyShort>
+        <ReadMore header={text(timestamp).card.readMoreTittel[language]}>
+          {text(timestamp).card.readMoreInnhold[language]}
         </ReadMore>
       </div>
     </div>
