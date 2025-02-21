712b791f131cb415ffedc41fcca6683445dfa66f
erikbosnes <erik.bosnes@nav.no>
2025-02-21 09:59:18 +0100

    Oppdater engelsk tekst

--- src/translations/text.ts
+++ src/translations/text.ts
@@ -8,17 +8,17 @@ export const cardText = () => ({
 
 export const descriptionText = (dato: string) => ({
   description: {
-    nn: `Nav starta deg på arbeidsretta oppfølging ${dato}`,
-    nb: `Nav startet deg på arbeidsrettet oppfølging ${dato}`,
-    en: `Nav started you on continued employment guidance ${dato}`,
+    nn: `Nav starta deg på arbeidsretta oppfølging ${dato}.`,
+    nb: `Nav startet deg på arbeidsrettet oppfølging ${dato}.`,
+    en: `Nav started you on continued employment guidance ${dato}.`,
   },
 });
 
 export const readMoreTittelText = () => ({
   readMoreTittel: {
-    nn: "Slik brukte me personopplysningane dine.",
-    nb: "Slik brukte vi personopplysningene dine.",
-    en: "This is how we used your personal data.",
+    nn: "Slik brukte me personopplysningane dine",
+    nb: "Slik brukte vi personopplysningene dine",
+    en: "This is how we used your personal data",
   },
 });
 
@@ -26,6 +26,6 @@ export const readMoreInnholdText = () => ({
   readMoreInnhold: {
     nn: "Då me starta deg på arbeidsretta oppfølging behandla me opplysningar om din alder, adresse og om du har lovleg opphald i Noreg. Dette gjorde me for å vera sikre på at du har krav på oppfølging, og for at du skal få oppfølging frå rett Nav kontor. Denne behandlinga er basert på Nav-loven § 14 a.",
     nb: "Da vi registrerte deg behandlet vi opplysninger om din alder, adresse og om du har lovlig opphold i Norge. Dette gjorde vi for å være sikre på at du har krav på oppfølging, og for at du skal få oppfølging fra riktig Nav kontor. Denne behandlingen er basert på Nav-loven § 14a.",
-    en: "When we started you on continued employment guidance, we processed information about your age, address and whether you have legal residence in Norway.  We did this to ensure that you are entitled to guidance, and to ensure that you receive guidance from the correct Nav office. This processing is based on Section 14 a of the Nac Act.",
+    en: "When we started you on continued employment guidance, we processed information about your age, address and whether you have legal residence in Norway.  We did this to ensure that you are entitled to guidance, and to ensure that you receive guidance from the correct Nav office. This processing is based on Section 14 a of the Nav Act.",
   },
 });
