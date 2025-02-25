a95f5da7aeba803a65101140f7bed7e9f7122527
erikbosnes <erik.bosnes@nav.no>
2025-02-25 09:59:22 +0100

    Legg til bruk av Umami nytt forsøk

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "@navikt/ds-css";
 import Komponent from "./components/ArbeidsrettetOppfolgingPanel";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <section>
+      <Helmet>
+        <script
+          defer
+          src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
+          data-host-url="https://umami.nav.no"
+          data-website-id="b0ce7216-d476-41d3-80c0-19a5f137d9f2"
+        ></script>
+      </Helmet>
       <Komponent />
     </section>
   );
