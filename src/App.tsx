04cd396b231c7c0b5949b093e7f1a7b958c8b7d8
erikbosnes <erik.bosnes@nav.no>
2025-02-25 10:14:12 +0100

    Legg til bruk av Umami manuelt

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import "@navikt/ds-css";
 import Komponent from "./components/ArbeidsrettetOppfolgingPanel";
-import { Helmet } from "react-helmet";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.nav.no/team-researchops/sporing/sporing.js";
+    script.defer = true;
+    script.setAttribute("data-host-url", "https://umami.nav.no");
+    script.setAttribute("data-website-id", "b0ce7216-d476-41d3-80c0-19a5f137d9f2");
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <section>
-      <Helmet>
-        <script
-          defer
-          src="https://cdn.nav.no/team-researchops/sporing/sporing.js"
-          data-host-url="https://umami.nav.no"
-          data-website-id="b0ce7216-d476-41d3-80c0-19a5f137d9f2"
-        ></script>
-      </Helmet>
       <Komponent />
     </section>
   );
