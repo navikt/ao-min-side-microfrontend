80ed7f7fdd5c58e9c1b9f0e42ff6937d2ebbf5b4
erikbosnes <erik.bosnes@nav.no>
2025-02-26 10:32:29 +0100

    Legg til tester for ArbeidsrettetOppfolgingPanel og cleanup

--- src/components/Komponent.test.tsx
+++
@@ -1,6 +0,0 @@
-// describe("Simple working test for Komponent", () => {
-//   it("has a text", () => {
-//     render(<Komponent tekst="tekst" />);
-//     expect(screen.getByText(/tekst/)).toBeDefined();
-//   });
-// });
---
+++ src/tests/ArbeidsrettetOppfolgingPanel.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "../utils/test-utils";
+import ArbeidsrettetOppfolgingPanel from "../components/ArbeidsrettetOppfolgingPanel";
+import { VeilarboppfolgingApi } from "../api/veilarboppfolging";
+import { act } from "react-dom/test-utils";
+import { waitFor } from "@testing-library/react";
+
+describe("Tests for ArbeidsrettetOppfolgingPanel", () => {
+  it("should have a headline", () => {
+    render(<ArbeidsrettetOppfolgingPanel />);
+    expect(screen.getByText("Du er registrert for arbeidsrettet oppfølging")).toBeDefined();
+  });
+
+  it("should have a description", () => {
+    render(<ArbeidsrettetOppfolgingPanel />);
+    expect(screen.getByText(/Nav startet deg på arbeidsrettet oppfølging/)).toBeDefined();
+  });
+
+  it("description should show date", async () => {
+    VeilarboppfolgingApi.hentGjeldendeOppfolgingsperiode = async () => "2025-01-01T12:00:00";
+
+    await act(async () => {
+      render(<ArbeidsrettetOppfolgingPanel />);
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(/onsdag 1. januar 2025/)).toBeDefined();
+    });
+  });
+
+  it("error fetching date should show missing date message", () => {
+    render(<ArbeidsrettetOppfolgingPanel />);
+    expect(screen.getByText(/(fant ikke dato)/)).toBeDefined();
+  });
+
+  it("should have a read more title", () => {
+    render(<ArbeidsrettetOppfolgingPanel />);
+    expect(screen.getByText("Slik brukte vi personopplysningene dine")).toBeDefined();
+  });
+
+  it("should have a read more content", () => {
+    render(<ArbeidsrettetOppfolgingPanel />);
+    expect(screen.getByText(/Da vi registrerte deg behandlet vi opplysninger om din alder/)).toBeDefined();
+  });
+});
