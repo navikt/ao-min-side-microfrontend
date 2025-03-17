08f27cad9f596a8d08e5e33e60bf8153dc5e689c
erikbosnes <erik.bosnes@nav.no>
2025-03-17 14:41:56 +0100

    Oppdatere tekster for bokmål, nynorsk, engelsk

--- src/translations/text.ts
+++ src/translations/text.ts
@@ -1,45 +1,45 @@
 export const descriptionText = (dato: string) => ({
   description: {
-    nn: `Da Nav registrerte deg for arbeidsretta oppfølging ${dato}`,
-    nb: `Da Nav registrerte deg for arbeidsrettet oppfølging ${dato}, måtte vi behandle dine personopplysninger`,
-    en: `Nav started you on continued employment guidance ${dato}`,
+    nb: `Nav registrerte deg for arbeidsrettet oppfølging ${dato}. For å kunne registrere deg måtte vi behandle personopplysningene dine.`,
+    nn: `Nav registrerte deg for arbeidsretta oppfølging ${dato}. For å kunna registrera deg måtte me behandla personopplysningane dine.`,
+    en: `Nav registered you for continued employment guidance ${dato}. In order to register you, we had to process your personal data.`,
   },
 });
 
 export const text = {
   heading: {
-    nn: "Arbeidsretta oppfølging",
     nb: "Arbeidsrettet oppfølging",
+    nn: "Arbeidsretta oppfølging",
     en: "Continued employment guidance",
   },
   readMoreTittel: {
-    nn: "Slik brukte me personopplysningane dine",
     nb: "Slik brukte vi personopplysningene dine",
-    en: "This is how we used your personal data",
+    nn: "Slik brukte me personopplysningane dine",
+    en: "How we used your personal information",
   },
   readMoreInnhold: {
-    nn: "Då me starta deg på arbeidsretta oppfølging behandla me opplysningar om din alder, adresse og om du har lovleg opphald i Noreg. Dette gjorde me for å vera sikre på at du har krav på oppfølging, og for at du skal få oppfølging frå rett Nav kontor. Denne behandlinga er basert på Nav-loven § 14 a.",
-    nb: "Da vi registrerte deg behandlet vi opplysninger om din alder, adresse og om du har lovlig opphold i Norge. Dette gjorde vi for å være sikre på at du har krav på oppfølging, og for at du skal få oppfølging fra riktig Nav kontor. Denne behandlingen er basert på Nav-loven § 14 a.",
-    en: "When we started you on continued employment guidance, we processed information about your age, address and whether you have legal residence in Norway.  We did this to ensure that you are entitled to guidance, and to ensure that you receive guidance from the correct Nav office. This processing is based on Section 14 a of the Nav Act.",
+    nb: "Vi behandlet opplysninger om din alder, adresse og om du har lovlig opphold i Norge.Dette gjorde vi for å være sikre på at du har krav på oppfølging, og for at du skal få oppfølging fra riktig Nav kontor. Denne behandlingen er basert på Nav-loven § 14 a.",
+    nn: "Me behandla opplysningar om din alder, adresse og om du har lovleg opphold i Norge. Dette gjorde me for å vera sikre på at du har krav på oppfølging, og for at du skal få oppfølging frå rett Nav kontor. Denne behandlinga er basert på Nav-loven § 14 a.",
+    en: "We processed information about your age, address and whether you have legal residence in Norway. We did this to ensure that you are entitled to guidance, and to ensure that you receive guidance from the correct Nav office. This processing is based on the Nav Act, Section 14 a.",
   },
   dialogTittel: {
-    nn: "Dialog med rettleiaren din",
     nb: "Dialog med veilederen din",
+    nn: "Dialog med rettleiaren din",
     en: "Dialogue with your advisor",
   },
   dialogTekst: {
-    nn: "Spør til dømes om avtalte møter, aktivitetar og tiltak",
-    nb: "Spør for eksempel om avtalte møter, aktiviteter og tiltak",
-    en: "Contact your advisor, for example regarding agreed meetings, activities and measures",
+    nb: "Spør f.eks. om avtalte møter og tiltak",
+    nn: "Spør til dømes om avtalte møter og tiltak",
+    en: "Regarding, for example, agreed meetings and activities",
   },
   aktivitetsplanTittel: {
-    nn: "Aktivitetsplan",
     nb: "Aktivitetsplan",
+    nn: "Aktivitetsplan",
     en: "Activity plan",
   },
   aktivitetsplanTekst: {
-    nn: "Oversikt over dine aktivitetar og tiltak",
-    nb: "Oversikt over dine aktiviteter og tiltak",
-    en: "Overview of the activities you are doing to find work",
+    nb: "Oversikt over dine aktiviteter, møter og samtalereferat",
+    nn: "Oversikt over aktivitetane dine, møte og samtalereferat",
+    en: "Overview of your activities, meetings and meeting summaries",
   },
 };
