1c4c3f57cdd9b75593789e1201bb39a7309162fc
johannetronstad <johanne.tronstad@nav.no>
2025-05-27 11:51:42 +0200

    Add space

--- src/translations/text.ts
+++ src/translations/text.ts
@@ -18,7 +18,7 @@ export const text = {
     en: "How we used your personal information",
   },
   readMoreInnhold: {
-    nb: "Vi behandlet opplysninger om din alder, adresse og om du har lovlig opphold i Norge.Dette gjorde vi for å være sikre på at du har krav på oppfølging, og for at du skal få oppfølging fra riktig Nav kontor. Denne behandlingen er basert på Nav-loven § 14 a.",
+    nb: "Vi behandlet opplysninger om din alder, adresse og om du har lovlig opphold i Norge. Dette gjorde vi for å være sikre på at du har krav på oppfølging, og for at du skal få oppfølging fra riktig Nav kontor. Denne behandlingen er basert på Nav-loven § 14 a.",
     nn: "Me behandla opplysningar om din alder, adresse og om du har lovleg opphold i Norge. Dette gjorde me for å vera sikre på at du har krav på oppfølging, og for at du skal få oppfølging frå rett Nav kontor. Denne behandlinga er basert på Nav-loven § 14 a.",
     en: "We processed information about your age, address and whether you have legal residence in Norway. We did this to ensure that you are entitled to guidance, and to ensure that you receive guidance from the correct Nav office. This processing is based on the Nav Act, Section 14 a.",
   },
